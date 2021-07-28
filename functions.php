0738e65a7a4b49a128d4be751193e1ccc79911c6
AndrAthena <andrianantenaina.nandrianina@gmail.com>
2021-07-28 17:47:18 +0300

    shortcode

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$includes = array( '/inc/theme-setup.php', '/inc/hook.php', '/inc/enqueue.php', '/inc/custom_cpt.php', '/inc/widget.php' );
+$includes = array( '/inc/theme-setup.php', '/inc/hook.php', '/inc/enqueue.php', '/inc/custom_cpt.php', '/inc/widget.php', '/inc/shortcode.php' );
 
 foreach ($includes as $include) {
   $include = locate_template( $include );
