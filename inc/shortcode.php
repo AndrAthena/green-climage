03d6dc113743e51519e34e6a84ed39e3ee02e5bb
AndrAthena <andrianantenaina.nandrianina@gmail.com>
2021-07-28 18:05:34 +0300

    fix agenda

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -32,7 +32,14 @@ function green_climate_listing_agenda() {
       if( has_excerpt() ) $output .= '<p>' .  wp_trim_excerpt( get_the_excerpt() ) . '</p>';
       else $output .= '<p>' . wp_trim_words( get_the_content(), 20 ) . '</p>';
       
-      $output .= green_climate_link_to( 'Voir plus', get_the_permalink() );
+      $output .='<a href="' . esc_url( get_the_permalink() )  .  '" class="d-inline-flex align-items-center ml-auto font-weight-bold">';
+      $output .= '<span class="mr-2">Voir plus</span>';
+      $output .= '<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">';
+      $output .= '<path d="M8.97733 17.9547C13.9354 17.9547 17.9547 13.9354 17.9547 8.97733C17.9547 4.01929 13.9354 0 8.97733 0C4.01929 0 0 4.01929 0 8.97733C0 13.9354 4.01929 17.9547 8.97733 17.9547Z" fill="#E68007"/>';
+      $output .= '<path d="M7.88917 4.48865L6.39294 5.98487L9.43073 8.97731L6.39294 12.0151L7.88917 13.5113L12.4232 8.97731L7.88917 4.48865Z" fill="white"/>';
+      $output .= '</svg>';
+      $output .= '</a>';
+      
       $output .= '    </div>';
       $output .= '  </div>';
       $output .= '</div>';
